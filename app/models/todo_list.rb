4b223b5b739fc8f1beb32239f3ccd634e134ff5e
JBNascimento <jbnascimento@msn.com>
2019-05-12 20:28:37 -0300

    Adicionadas validações de entrada  nas listas e tarefas

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,4 +2,7 @@ class TodoList < ApplicationRecord
     has_many :todo_items, dependent: :destroy
     belongs_to :user 
     has_many :favorites, dependent: :destroy
+
+    validates :title, presence: true
+    validates :description, presence: true
 end
